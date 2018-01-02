89b9f0d69fce7b89133661d770215f70b003b7a9
Philip Helger <philip@helger.com>
2018-01-02 17:15:16 +0100

    More default class path entries

--- src/main/java/org/codehaus/mojo/javacc/ForkedJvmPGCC.java
+++ src/main/java/org/codehaus/mojo/javacc/ForkedJvmPGCC.java
@@ -1,5 +1,9 @@
 package org.codehaus.mojo.javacc;
 
+import javax.annotation.Nullable;
+
+import org.slf4j.LoggerFactory;
+
 import com.helger.commons.id.IHasID;
 import com.helger.security.messagedigest.EMessageDigestAlgorithm;
 
@@ -13,5 +17,7 @@ class ForkedJvmPGCC extends ForkedJvm
   {
     addClassPathEntry (IHasID.class);
     addClassPathEntry (EMessageDigestAlgorithm.class);
+    addClassPathEntry (LoggerFactory.class);
+    addClassPathEntry (Nullable.class);
   }
 }
